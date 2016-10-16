47a6c0e950266cda69bf3ddbaacdab7c733a2a97
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-10-16 12:47:01 -0700

    Remove AngularJS

--- static/js/page/main/mainPageApp.js
+++
@@ -1,9 +0,0 @@
-angular.module('brok.page.main', []);
-
-var brok = brok || {};
-brok.page = brok.page || {};
-brok.page.main = (function() {	
-	var publicObjects = {}
-
-	return publicObjects;
-}());
